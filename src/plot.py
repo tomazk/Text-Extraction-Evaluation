789c007bdb2e86893d85191670a327dc1989ea2b
Tomaz Kovacic <tomaz.kovacic@gmail.com>
2011-05-09 18:24:03 +0200

    Added: 3d bar chart for individual extractors

--- src/plot.py
+++ src/plot.py
@@ -1,19 +1,14 @@
 import os
+import math
 
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 import settings
 from txtexeval.evaluation import TextBasedResults
-from txtexeval.extractor import extractor_list
-
-#helper
-def autolabel(rects):
-    for rect in rects:
-        height = rect.get_height()
-        plt.text(rect.get_x()+rect.get_width()/2.25,rect.get_height() + 0.01 ,
-                 '%1.2f'%height, ha='center', va='bottom', size = 'x-small')
+from txtexeval.extractor import extractor_list, get_extractor_cls
 
 def precision_recall_plot(dataset_name, img_name):
 
@@ -46,20 +41,21 @@ def precision_recall_plot(dataset_name, img_name):
         plt.subplot(3,1,i+1)
         plt.grid(True, alpha = 0.5)
         
-        rects_avg = plt.bar(ind, avg, width,color=bar_color[i], ecolor ='g' ,yerr = stddev)
+        rects_avg = plt.bar(ind, avg, width,color=bar_color[i], ecolor ='g' ,
+            yerr = stddev, linewidth = 0.5, alpha = 0.8)
         
         # lables and titles
         plt.title(pdata[0])
-        
         plt.xticks(ind+width/2., extractor_names, size = 'xx-small', rotation = 'vertical')
-        
         plt.legend( (rects_avg[0],),
                      ('avg',),
                      fancybox = True,
                      prop = dict(size='x-small')
         )
-        
-        autolabel(rects_avg)
+        for rect in rects_avg:
+            height = rect.get_height()
+            plt.text(rect.get_x()+rect.get_width()/2.25,rect.get_height() + 0.01 ,
+                 '%1.2f'%height, ha='center', va='bottom', size = 'x-small')
         
         
     #subplots adjusting
@@ -72,11 +68,77 @@ def precision_recall_plot(dataset_name, img_name):
     
     # output 
     out_path = os.path.join(settings.PATH_LOCAL_DATA, 'plot-output', img_name)
-    plt.savefig( out_path )
-
-
+    plt.savefig(out_path)
+    
+def equidistant_count(start, stop, step , list):
+    limit_list = np.arange(start,stop, step)
+    count = []
+    for low in limit_list:
+        up = low + step
+        bmap = map(lambda x: 1 if low <= x < up else 0 , list)
+        count.append(sum(bmap))
+    return tuple(count)
+        
+    
+def extractor_stat_plot(dataset_name, extractor_slug):
+    plt.clf() # clear current figure
+    extractor_cls = get_extractor_cls(extractor_slug)
+    
+    # get results and repackage the data
+    txt_results = TextBasedResults()
+    txt_results.load(dataset_name)
+    txt_results.print_results()
+    extractor_results = txt_results.get_results()[extractor_cls.NAME]
+    
+    non_inf_nan = lambda r: (not math.isinf(r)) and (not math.isnan(r))
+    results_list_prec = filter(non_inf_nan, [r[0] for r in extractor_results]) 
+    results_list_rec = filter(non_inf_nan,[r[1] for r in extractor_results])
+    results_list_f1 = filter(non_inf_nan,[r[0] for r in extractor_results ])
+    
+    width = 0.1  # the width of the bars
+    ind = np.arange(0,1,0.1)
+    eq_count_prec = equidistant_count(0, 1, 0.1, results_list_prec)
+    eq_count_rec = equidistant_count(0, 1, 0.1, results_list_rec)
+    eq_count_f1 = equidistant_count(0, 1, 0.1, results_list_f1)
+    
+    # plotting
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection = '3d')
+    
+    ax.bar( ind, eq_count_prec, zs = 0 ,zdir='y', width = width,color='b',
+                          linewidth = 0.5, alpha = 0.8)
+    ax.bar( ind, eq_count_rec, zs = 1 , zdir='y', width = width,color='m',
+                          linewidth = 0.5, alpha = 0.8)
+    ax.bar( ind, eq_count_f1, zs = 2 , zdir='y', width = width,color='c',
+                          linewidth = 0.5, alpha = 0.8)
+    
+    plt.title(extractor_cls.NAME)
+    ax.set_xlabel('limits')
+    ax.set_zlabel('no. of instances')
+    ax.yaxis.set_ticks([])
+    ax.grid(True, alpha = 0.7)
+    
+    # with 3d plotting we need to use proxy artist because legends
+    # are not supported
+    blue = plt.Rectangle((0, 0), 1, 1, fc="b") # proxys
+    cyan = plt.Rectangle((0, 0), 1, 1, fc="c")
+    mag = plt.Rectangle((0, 0), 1, 1, fc="m")
+    plt.legend(      (blue,mag,cyan),
+                     ('precision','recall','f1 score'),
+                     fancybox = True,
+                     prop = dict(size='x-small')
+    )
+    
+    # save plot
+    out_path = os.path.join(settings.PATH_LOCAL_DATA, 'plot-output', 
+                            '%s.png' % extractor_slug)
+    plt.savefig(out_path)
+    
+    
+ 
 def parse_args():
     parser = argparse.ArgumentParser(description = 'Plotting tool')
+    parser.add_argument('action', choices = ('dataset_stat', 'extr_stat'))
     parser.add_argument('dataset_name', help = 'name of the dataset')
     parser.add_argument('-o','--output_img_name', type=str, help = 'name of the output image')
     return parser.parse_args()
@@ -84,8 +146,12 @@ def parse_args():
 def main():
     args = parse_args()
     
-    output_img_name = args.output_img_name or '%s.png' % args.dataset_name
-    precision_recall_plot(args.dataset_name, output_img_name)
+    if args.action == 'dataset_stat':
+        output_img_name = args.output_img_name or '%s.png' % args.dataset_name
+        precision_recall_plot(args.dataset_name, output_img_name)
+    elif args.action == 'extr_stat':
+        # TODO: loop over all extractors
+        extractor_stat_plot(args.dataset_name, 'zemanta')
     
     print '[DONE]'
 
